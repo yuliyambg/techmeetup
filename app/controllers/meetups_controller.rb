75407fea7e0555608eb7ee49478cfe61cf75769b
Yuliya Marinova <65470176+yuliyambg@users.noreply.github.com>
2021-03-31 02:34:38 +0000

    meetup index and show action and view

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,2 +1,13 @@
 class MeetupsController < ApplicationController
+
+    def index
+       @meetups = Meetup.all
+    end
+
+    def show
+       @meetup = Meetup.where(id: params[:id]).first
+       
+    end
+
+
 end
