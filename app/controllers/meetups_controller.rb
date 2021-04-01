c303c79e5f1957ba0f548e9967835f4a089d2a59
Yuliya Marinova <65470176+yuliyambg@users.noreply.github.com>
2021-04-01 01:22:57 +0000

    creating new meetup

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -10,5 +10,19 @@ class MeetupsController < ApplicationController
        @comments = @meetup.comments
     end
 
+    def new
+    end
+
+    def create
+      # raise params.inspect
+      title = params[:meetup][:title]
+      date_time = params[:meetup][:date_time]
+      description = params[:meetup][:description]
+      image_url = params[:meetup][:image_url]
+      link = params[:meetup][:link]
+      @meetup = Meetup.create(title: title, date_time: date_time, description: description, image_url: image_url, link: link)
+  
+      redirect_to :meetups
+    end
 
 end
