a8931b331246050688c1e5f1660dc2306bd14382
Yuliya Marinova <65470176+yuliyambg@users.noreply.github.com>
2021-04-06 23:23:55 +0000

    adding root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'meetups#index'
   get 'login', to: 'session#new'
   post 'login', to: 'session#create'
   delete 'session', to: 'session#destroy'
