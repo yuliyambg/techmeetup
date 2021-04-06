b9c1992c636feb26dc3f3b3e3b771dba4eaedabe
Yuliya Marinova <65470176+yuliyambg@users.noreply.github.com>
2021-04-06 23:19:36 +0000

    adding popular events

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -7,6 +7,10 @@ class Meetup < ApplicationRecord
         Meetup.all.order(date_time: :desc).limit(3)
     end
 
+    def self.popular_events
+        joins(:meetups_users).group("meetups.title").order(count_all: :desc).limit(3)
+    end
+
     def self.search_meetups(search_text)
         (Meetup.where("title LIKE '%#{search_text}%'"))
     end
