4c17ee1ca79d06bcee030f0abdf2807974143f13
Yuliya Marinova <65470176+yuliyambg@users.noreply.github.com>
2021-04-09 22:58:42 +0000

    incorporate weather api

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,20 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+function getWeather() {
+    navigator.geolocation.getCurrentPosition(onSuccess);    
+  }
+  
+function onSuccess(pos) {
+    const weatherText = document.getElementById('weather');
+  
+    
+    const uri = `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=25179022956c5c92b9b92d55bbf284f3&units=imperial`
+    fetch(uri).then(res => res.json())
+        .then(data => {
+          weatherText.innerHTML = data.name + ": " + data.main.temp + "&deg; F";
+        })
+  }
+  
+  document.addEventListener('DOMContentLoaded', getWeather);
