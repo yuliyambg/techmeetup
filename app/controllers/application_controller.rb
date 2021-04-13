37192f9ab78bb8d8c36274bf1357b009f862bace
Yuliya Marinova <65470176+yuliyambg@users.noreply.github.com>
2021-04-13 03:50:25 +0000

    adding upcoming_meetups and popular_events to helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
-    helper_method :upcoming_meetups
+    # helper_method :upcoming_meetups
 
-    def upcoming_meetups
-      @upcoming_meetups = Meetup.upcoming_meetups
-    end
+    # def upcoming_meetups
+    #   @upcoming_meetups = Meetup.upcoming_meetups
+    # end
     
     def current_user
       @current_user ||= User.find_by(id: session[:user_id])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def upcoming_meetups
+        Meetup.all.order(date_time: :desc).limit(3)
+    end
+
+    def popular_events
+        Meetup.joins(:meetups_users).group("meetups.title").order(count_all: :desc).limit(3)
+    end
+
 end
