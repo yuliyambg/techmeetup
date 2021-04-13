8a2cdaa18775bf67ade8212cbd2adb32901f5d7a
Yuliya Marinova <65470176+yuliyambg@users.noreply.github.com>
2021-04-13 05:01:12 +0000

    adding user_id to comments table, adding deatils to comments created by and created_at

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
     belongs_to :meetup
-    # belongs_to :user
+    belongs_to :user
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_232741) do
+ActiveRecord::Schema.define(version: 2021_04_13_035434) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "meetup_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
