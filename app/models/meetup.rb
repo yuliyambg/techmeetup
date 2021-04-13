6909979185386d4aa6698929640ee5a2fbff608c
Yuliya Marinova <65470176+yuliyambg@users.noreply.github.com>
2021-04-13 03:46:17 +0000

    implementing scope for search_meetups

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -3,16 +3,16 @@ class Meetup < ApplicationRecord
     has_many :meetups_users
     has_many :users, through: :meetups_users
 
-    def self.upcoming_meetups
-        Meetup.all.order(date_time: :desc).limit(3)
-    end
+    scope :search_meetups, -> (search_text) {  where("title LIKE ?", "#{search_text}%") }
 
-    def self.popular_events
-        joins(:meetups_users).group("meetups.title").order(count_all: :desc).limit(3)
-    end
+    # def self.upcoming_meetups
+        # Meetup.all.order(date_time: :desc).limit(3)
+    # end
 
-    def self.search_meetups(search_text)
-        (Meetup.where("title LIKE '%#{search_text}%'"))
-    end
+    # def self.popular_events
+    #     joins(:meetups_users).group("meetups.title").order(count_all: :desc).limit(3)
+    # end
+
+
 
 end
